92777f02309407aa976f2fa246ead8ec055b3d91
Khoa Le <khoatle165@gmail.com>
2019-08-07 15:29:59 +0200

    migrate and add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+12.times do
+  Flat.create(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: Random.rand(3..5), supplemental: true, random_sentences_to_add: 5),
+    price_per_night: Random.rand(30..100),
+    number_of_guests: Random.rand(1..5)
+  )
+end
+
+puts 'Finished!'
